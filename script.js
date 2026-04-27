7f45f85e0d064a10990c226fd480e4e9e4e44cc2
Anil Sapkota <muzicmaniac2015@gmail.com>
2026-04-27 17:08:31 -0500

    basic stuff to make map load

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const map = L.map('map').setView([20, 0], 2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
