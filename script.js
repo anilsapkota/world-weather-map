6786252bd62815999f2c2830fa5e219c38689549
Anil Sapkota <muzicmaniac2015@gmail.com>
2026-04-27 20:31:20 -0500

    wrote the code to get the current_weather info based on the click which gives the temperature and condition as well

--- script.js
+++ script.js
@@ -7,5 +7,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //adding the logic for clicking event in the map with built in from leaflet
 map.on('click', function(e) {
-    console.log(e.latlng);
+    //console.log(e.latlng);
+    //getting the lattitude and longitude from the map 
+    const lat = e.latlng.lat
+    const lng = e.latlng.lng
+
+    //pass this lat and lng to the openmateo for weather
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current_weather=true`;
+
+    fetch(url)
+        .then(response =>response.json())
+        .then(data =>{
+            console.log(data.current_weather)
+
+        });
 });
